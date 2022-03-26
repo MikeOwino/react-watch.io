3fdda12fc951fd50edca6496a308a14d87fd2d03
Mike Owino <mike@lancolatech.co.ke>
2022-03-26 11:24:21 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -3,22 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <video id="my-video" className="video-js" controls preload="auto" poster="https://res.cloudinary.com/weknow-creators/image/upload/c_scale,w_1280/v1647843623/wallhaven-83g6zy_i52wko.png" data-setup loop>
+        <source src="https://res.cloudinary.com/weknow-creators/video/upload/v1647855818/1434659607842-pgv4ql-1647443114902_xvz1al.mp4" type="video/mp4" />
+      </video>
   );
 }
 
